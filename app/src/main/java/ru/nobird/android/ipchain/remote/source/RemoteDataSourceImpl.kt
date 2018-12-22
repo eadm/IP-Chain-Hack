efcbb74caef59b7019a1f7d31c85b73c8d06bf88
Ignat Loskutov <ignat.loskutov@gmail.com>
2018-12-22 18:55:45 +0300

    Implement RemoteDataSourceImpl

--- app/src/main/java/ru/nobird/android/ipchain/remote/source/RemoteDataSourceImpl.kt
+++ app/src/main/java/ru/nobird/android/ipchain/remote/source/RemoteDataSourceImpl.kt
@@ -1,4 +1,28 @@
 package ru.nobird.android.ipchain.remote.source
 
-class RemoteDataSourceImpl {
+import io.reactivex.Single
+import ru.nobird.android.ipchain.remote.model.*
+import ru.nobird.android.ipchain.remote.service.RestService
+import javax.inject.Inject
+
+class RemoteDataSourceImpl
+@Inject
+constructor(
+    private val restService: RestService
+) {
+
+    fun postTransaction(channelId: String, transaction: Transaction): Single<TransactionRegistred> =
+        restService.postTransaction(channelId, transaction)
+
+    fun getTransaction(transactionId: String, channelId: String): Single<Transaction> =
+        restService.getTransaction(transactionId, channelId)
+
+    fun getTransactionByObject(objectId: String, channelId: String): Single<ObjectDataTransactions> =
+        restService.getTransactionByObject(objectId, channelId)
+
+    fun getObjectsByName(name: String): Single<List<ObjectData>> =
+        restService.getObjectsByName(name)
+
+    fun getObjectTypes(): Single<List<String>> =
+        restService.getObjectTypes()
 }
