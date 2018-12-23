f6dda52ddaa78d7a493997c878fe91d350794e32
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-12-23 06:25:25 +0300

    get results

--- app/src/main/java/ru/nobird/android/ipchain/view/patents/ui/activity/PatentsActivity.kt
+++ app/src/main/java/ru/nobird/android/ipchain/view/patents/ui/activity/PatentsActivity.kt
@@ -5,11 +5,15 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_patents.*
 import ru.nobird.android.ipchain.App
 import ru.nobird.android.ipchain.R
 import ru.nobird.android.ipchain.presentation.patents.PatentsPresenter
+import ru.nobird.android.ipchain.view.patent.model.Download
+import ru.nobird.android.ipchain.view.patent.model.Item
 import ru.nobird.android.ipchain.view.patent.ui.activity.PatentActivity
+import ru.nobird.android.ipchain.view.patent.ui.adapter.DownloadAdapter
 import javax.inject.Inject
 
 class PatentsActivity : AppCompatActivity() {
@@ -19,6 +23,8 @@ class PatentsActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private val adapter = DownloadAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patents)
@@ -27,7 +33,17 @@ class PatentsActivity : AppCompatActivity() {
         presenter = ViewModelProviders.of(this, viewModelFactory).get(PatentsPresenter::class.java)
 
         addPatent.setOnClickListener {
-            startActivity(Intent(this, PatentActivity::class.java))
+            startActivityForResult(Intent(this, PatentActivity::class.java), PatentActivity.RQ)
+        }
+
+        patents.adapter = adapter
+        patents.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        data?.getParcelableExtra<Item>("item")?.let {
+            adapter.downloads.add(Download(it.title, it.date))
+            adapter.notifyItemInserted(adapter.downloads.size - 1)
         }
     }
 
---
+++ app/src/main/java/ru/nobird/android/ipchain/view/patents/ui/adapter/PatentsAdapter.kt
@@ -0,0 +1,1 @@
+package ru.nobird.android.ipchain.view.patents.ui.adapter
